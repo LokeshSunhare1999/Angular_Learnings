5c90c8c768a08dd51df06a7ee3902186d910a931
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2026-04-14 22:00:07 +0530

    added first file

--- angular-21-tut/src/app/app.ts
+++ angular-21-tut/src/app/app.ts
@@ -4,9 +4,15 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
+  templateUrl: './first.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-21-tut');
+  name="Lokesh";
+  age = "25";
+
+  updateName(){
+    this.name = "20"; // can't assign number bcoz initially it is string and type of variable can't be changed in typescript
+  }
 }
