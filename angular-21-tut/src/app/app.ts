57f9e8993aedeb18faf64e3625f3ea12a38c2485
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2026-04-19 18:12:03 +0530

    try components

--- angular-21-tut/src/app/app.ts
+++ angular-21-tut/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Profile } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './first.html',
+  imports: [RouterOutlet, Profile],
+  templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
