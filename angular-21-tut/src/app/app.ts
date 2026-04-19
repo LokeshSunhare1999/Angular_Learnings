52c8bc6be26d945ecb19a3e7eeb02b9db5e3f60b
Lokesh Sunhare <lokesh1999sunhare@gmail.com>
2026-04-19 18:57:31 +0530

    learnt event binding

--- angular-21-tut/src/app/app.ts
+++ angular-21-tut/src/app/app.ts
@@ -6,14 +6,27 @@ import { Profile } from './profile/profile';
   selector: 'app-root',
   imports: [RouterOutlet, Profile],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('angular-21-tut');
-  name="Lokesh";
-  age = "25";
+  name = 'Lokesh';
+  age = '25';
+count = 0;
+  counter(action: string) {
+    if (action === 'plus') {
+      this.count++;
+    } else if (action === 'minus') {
+      if (this.count > 0) {
+        this.count--;
+      }
+    }
+    console.log(this.count);
+    this.updateName();
+  
+  }
 
-  updateName(){
-    this.name = "20"; // can't assign number bcoz initially it is string and type of variable can't be changed in typescript
+  updateName() {
+    this.name = '20'; // can't assign number bcoz initially it is string and type of variable can't be changed in typescript
   }
 }
